91322bfead5407de3b5e1297b5bff6d95b51892a
Abhinov Dutta <abhinavdutta927@gmail.com>
2025-01-24 02:48:40 +0530

    Major Changes

--- src/routes/newsletter.js
+++ src/routes/newsletter.js
@@ -1,10 +1,32 @@
 const express = require('express');
-const { NewsLetterModel } = require('../config/schema');
+const { client } = require('../config/database');
 
 const newsletter = express.Router();
 
+newsletter.get('/:newsletterId', async (req, res) => {
+  const { newsletterId } = req.params; // Extract newsletterId from the URL parameters
+
+  try {
+    const result = await client.query(
+      `DELETE FROM Newsletter WHERE id = $1 RETURNING *`,
+      [newsletterId]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ msg: "Newsletter not found" });
+    }
+
+    res.status(200).json({ msg: "Newsletter deleted successfully", deletedNewsletter: result.rows[0] });
+  } catch (error) {
+    console.error('Error deleting newsletter:', error.message);
+    res.status(500).json({ msg: "Error while deleting newsletter" });
+  }
+});
+
+
 newsletter.post('/create',async(req,res)=>{
     const { title ,content } = req.body;
+    console.log(req.body);
     try {
         const result = await client.query(
           `INSERT INTO Newsletter (title, content) VALUES ($1, $2) RETURNING *`,
@@ -28,5 +50,6 @@ newsletter.get('/',async(req,res)=>{
         return;
       }
 });
+
 
 module.exports = newsletter ;
